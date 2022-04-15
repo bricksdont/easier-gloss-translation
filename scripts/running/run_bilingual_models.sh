407b366dee4f53209ab55fec6066f06a1821e9b5
Mathias Müller <mathias.mueller@uzh.ch>
2022-04-15 17:17:04 +0200

    runs for all bilingual models

---
+++ scripts/running/run_bilingual_models.sh
@@ -0,0 +1,88 @@
+#! /bin/bash
+
+base=/net/cephfs/shares/volk.cl.uzh/mathmu/easier-gloss-translation
+scripts=$base/scripts
+
+lowercase_glosses_options="true false"
+generalize_dgs_glosses_options="true false"
+spm_strategy_options="joint separate spoken-only"
+
+dry_run="true"
+
+## UHH
+
+training_corpora="uhh"
+
+# DGS -> German
+
+# Structure: [source corpus] [src] [trg]
+
+language_pairs=(
+    "uhh dgs_de de"
+)
+
+for lowercase_gloss in $lowercase_glosses_options; do
+    for generalize_dgs_glosses in $generalize_dgs_glosses_options; do
+        for spm_strategy in $spm_strategy_options; do
+
+            model_name="lowercase_gloss.$lowercase_gloss+generalize_dgs_glosses.$generalize_dgs_glosses+spm_strategy.$spm_strategy"
+
+            . $scripts/running/run_generic.sh
+
+        done
+    done
+done
+
+# German -> DGS
+
+language_pairs=(
+    "uhh de dgs_de"
+)
+
+for lowercase_gloss in $lowercase_glosses_options; do
+    for generalize_dgs_glosses in $generalize_dgs_glosses_options; do
+        for spm_strategy in $spm_strategy_options; do
+
+            model_name="lowercase_gloss.$lowercase_gloss+generalize_dgs_glosses.$generalize_dgs_glosses+spm_strategy.$spm_strategy"
+
+            . $scripts/running/run_generic.sh
+
+        done
+    done
+done
+
+## BSLCP
+
+training_corpora="bslcp"
+
+# BSL -> English
+
+language_pairs=(
+    "bslcp bsl en"
+)
+
+for lowercase_gloss in $lowercase_glosses_options; do
+    for spm_strategy in $spm_strategy_options; do
+
+        model_name="lowercase_gloss.$lowercase_gloss+spm_strategy.$spm_strategy"
+
+        . $scripts/running/run_generic.sh
+
+    done
+done
+
+# English -> BSL
+
+language_pairs=(
+    "bslcp en bsl"
+)
+
+for lowercase_gloss in $lowercase_glosses_options; do
+    for spm_strategy in $spm_strategy_options; do
+
+        model_name="lowercase_gloss.$lowercase_gloss+spm_strategy.$spm_strategy"
+
+        . $scripts/running/run_generic.sh
+
+    done
+done
