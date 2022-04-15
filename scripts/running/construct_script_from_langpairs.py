96e858488bf97be19fc03ddbab4677c082642b4e
Mathias Müller <mathias.mueller@uzh.ch>
2022-04-15 15:23:28 +0200

    more robust arg checking

--- scripts/running/construct_script_from_langpairs.py
+++ scripts/running/construct_script_from_langpairs.py
@@ -2,6 +2,9 @@
 
 import sys
 
+SOURCES = ["uhh", "bslcp"]
+LANGUAGES = ["de", "en", "bsl", "dgs_de", "dgs_en"]
+
 
 def chunks(lst, n):
     """
@@ -21,7 +24,24 @@ print("language_pairs=(")
 
 parts = input_string.split(" ")
 
+if len(parts) % 3 != 0:
+    sys.stderr.write("ERROR: Language pairs array is not well-formed: %s" % str(parts) + "\n")
+    sys.exit(1)
+
 for source, src, trg in chunks(parts, n=3):
+
+    if source not in SOURCES:
+        sys.stderr.write("ERROR: Source '%s' not in known sources: '%s'" % (source, str(SOURCES)) + "\n")
+        sys.exit(1)
+
+    if src not in LANGUAGES:
+        sys.stderr.write("ERROR: src '%s' not in known languages: '%s'" % (src, str(LANGUAGES)) + "\n")
+        sys.exit(1)
+
+    if trg not in LANGUAGES:
+        sys.stderr.write("ERROR: trg '%s' not in known languages: '%s'" % (trg, str(LANGUAGES)) + "\n")
+        sys.exit(1)
+
     print("    ", end="")
     print('"%s %s %s"' % (source, src, trg))
     pass
