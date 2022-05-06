d8de721f321a6bd25e7ebbefd76a9b736df0b214
Mathias Müller <mathias.mueller@uzh.ch>
2022-05-06 09:23:20 +0200

    package and deploy models

---
+++ scripts/deployment/upload_models.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+
+base=/net/cephfs/shares/volk.cl.uzh/mathmu/easier-gloss-translation
+
+scripts=$base/scripts
+models=$base/models
+shared_models=$base/shared_models
+deploy=$base/deploy
+
+mkdir -p $deploy
+
+model_name="multilingual.true+lg.true+gdg.true+ss.joint"
+
+# DGS
+
+deploy_name="dgs_de"
+langpair="uhh.dgs_de+uhh.de-uhh.de+uhh.dgs_de"
+
+. $scripts/deployment/upload_models_generic.sh
+
+# BSL
+
+deploy_name="bsL_en"
+langpair="bslcp.bsl+bslcp.en-bslcp.en+bslcp.bsl"
+
+. $scripts/deployment/upload_models_generic.sh
---
+++ scripts/deployment/upload_models_generic.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Calling script needs to set:
+# $models
+# $shared_models
+# $deploy
+# $deploy_name
+# $langpair
+# $model_name
+
+relevant_files="args.yaml config log params.best version vocab.src.0.json vocab.trg.0.json"
+relevant_files_spm="sentencepiece.model sentencepiece.vocab"
+
+models_sub=$models/$langpair/$model_name
+shared_models_sub=$shared_models/$langpair/$model_name
+deploy_sub=$deploy/$deploy_name
+
+mkdir -p $deploy_sub
+
+for f in $relevant_files; do
+    cp $models_sub/$f $deploy_sub/$f
+done
+
+for sf in $relevant_files_spm; do
+    cp $shared_models_sub/$sf $deploy_sub/$sf
+done
+
+rm -f $deploy_sub.tar.gz
+
+tar -czvf $deploy_sub.tar.gz $deploy_sub
+
+scp $deploy_sub.tar.gz mmueller@home.ifi.uzh.ch:/home/files/cl/archiv/2022/easier/$deploy_name.tar.gz
