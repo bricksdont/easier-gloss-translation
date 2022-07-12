a7cbad271140688cb22d591dda178dce387b032e
Mathias Müller <mathias.mueller@uzh.ch>
2022-07-12 14:49:50 +0200

    "hf pretrained dry run

---
+++ scripts/running/dry_run_hf.sh
@@ -0,0 +1,46 @@
+#! /bin/bash
+
+base=/net/cephfs/shares/volk.cl.uzh/mathmu/easier-gloss-translation
+scripts=$base/scripts
+
+# DGS -> German
+
+# Structure: [source corpus] [src] [trg]
+
+language_pairs=(
+    "uhh dgs_de de"
+)
+
+# dry runs of all steps
+
+dry_run="true"
+
+repeat_download_step="false"
+
+# pretrained baseline
+
+model_name="dry_run_hf"
+
+training_corpora="uhh"
+testing_corpora="test"
+
+pretrained="true"
+pretrained_model_name="mt5-small"
+
+# construct src and trg from language_pairs
+
+src=""
+trg=""
+
+for pair in "${language_pairs[@]}"; do
+    pair=($pair)
+
+    src=${src:+$src+}${pair[0]}.${pair[1]}
+    trg=${trg:+$trg+}${pair[0]}.${pair[2]}
+done
+
+# delete files for this model to rerun everything
+
+. $scripts/running/prompt_to_delete_dry_run_folders.sh
+
+. $scripts/running/run_generic.sh
