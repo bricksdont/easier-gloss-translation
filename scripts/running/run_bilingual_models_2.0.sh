788f5d51cedbac79e2eff4255e825321a49c596b
Mathias Müller <mathias.mueller@uzh.ch>
2022-08-04 14:33:15 +0200

    text-to-gloss no mouthing tier

--- scripts/running/run_bilingual_models_2.0.sh
+++ scripts/running/run_bilingual_models_2.0.sh
@@ -35,10 +35,6 @@ language_pairs=(
     "uhh de dgs_de"
 )
 
-for use_mouthing_tier in true false; do
+model_name="2.0"
 
-    model_name="2.0+use_mouthing_tier.$use_mouthing_tier"
-
-    . $scripts/running/run_generic.sh
-
-done
+. $scripts/running/run_generic.sh
