e9cd5212abfc8108785d38b21c3a4c8b48d654fb
Mathias Müller <mathias.mueller@uzh.ch>
2022-08-16 15:19:14 +0200

    DGS document split option

---
+++ scripts/running/run_dgs_document_split.sh
@@ -0,0 +1,42 @@
+#! /bin/bash
+
+base=/net/cephfs/shares/volk.cl.uzh/mathmu/easier-gloss-translation
+scripts=$base/scripts
+
+lowercase_glosses="true"
+generalize_dgs_glosses="true"
+spm_strategy="joint"
+
+dry_run="false"
+
+## UHH
+
+training_corpora="uhh"
+
+dgs_use_document_split="true"
+
+# DGS -> German
+
+# Structure: [source corpus] [src] [trg]
+
+language_pairs=(
+    "uhh dgs_de de"
+)
+
+for use_mouthing_tier in true false; do
+
+    model_name="dgs_use_document_split.true+version.2+use_mouthing_tier.$use_mouthing_tier"
+
+    . $scripts/running/run_generic.sh
+
+done
+
+# German -> DGS
+
+language_pairs=(
+    "uhh de dgs_de"
+)
+
+model_name="dgs_use_document_split.true+version.2"
+
+. $scripts/running/run_generic.sh
