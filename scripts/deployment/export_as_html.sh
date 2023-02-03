a1de080f3d2a8fa65c6e9c63db993624fdcad03a
Mathias Müller <mathias.mueller@uzh.ch>
2023-02-03 18:00:19 +0100

    debug export

--- scripts/deployment/export_as_html.sh
+++ scripts/deployment/export_as_html.sh
@@ -6,9 +6,21 @@ scripts=$base/scripts
 data=$base/data
 translations=$base/translations
 venvs=$base/venvs
+deploy=$base/deploy
+
+mkdir -p $deploy
+mkdir -p $deploy/html_export
 
 source activate $venvs/sockeye3
 
 python $scripts/deployment/export_as_html.py \
     --translations $translations/srf.dsgs-srf.de/emsl_v2a/srf.test.dsgs-de.de \
-    --references $data/srf.dsgs-srf.de/emsl_v2a/srf.test.de
+    --references $data/srf.dsgs-srf.de/emsl_v2a/srf.test.de \
+    > $deploy/html_export/emsl_v2a.html
+
+echo "Saved to"
+echo "$deploy/html_export/emsl_v2a.html"
+
+# upload
+
+# scp $deploy/html_export/emsl_v2a.html mmueller@home.ifi.uzh.ch:/home/files/cl/archiv/2023/easier/emsl_v2a.html
